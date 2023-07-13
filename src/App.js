c83183e7ddd5b2050b727648b1e6dee90c39a367
Aryan Jha <aryan2195jha@gmail.com>
2023-07-13 12:04:24 +0530

    navbar design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./component/Navbar.jsx";
+import NavBarr from "./component/NavBarr.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavBarr/>
     </div>
   );
 }
