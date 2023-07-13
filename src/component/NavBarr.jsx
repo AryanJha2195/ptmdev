7b61935f15e5b2346e9337cf6ce7c8f81509064e
Aryan Jha <aryan2195jha@gmail.com>
2023-07-14 03:20:08 +0530

    nav

--- src/component/NavBarr.jsx
+++ src/component/NavBarr.jsx
@@ -12,14 +12,14 @@ const NavBarr = () => {
           <img style={{width: '4rem', height: '4rem', borderRadius: '50px'}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrBUxPCkpN7dAMtx7DAJPxoXzUgJ4de0gii8eogEG-&s' alt='PTM'/>
           <Navbar.Brand href="#home"></Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">About Us</Nav.Link>
-            <Nav.Link href="#home">Insights</Nav.Link>
-            <Nav.Link href="#home">Parent Portal</Nav.Link>
-            <Nav.Link href="#features">Teacher Portal</Nav.Link>
-            <Nav.Link href="#pricing">Student Portal</Nav.Link>
+            <Nav.Link href="#about us">About Us</Nav.Link>
+            <Nav.Link href="#insights">Insights</Nav.Link>
+            <Nav.Link href="#parent">Parent Portal</Nav.Link>
+            <Nav.Link href="#teacher">Teacher Portal</Nav.Link>
+            <Nav.Link href="#student">Student Portal</Nav.Link>
           </Nav>
           <Nav className=''>
-          <Nav.Link href="#home">Log In</Nav.Link>
+          <Nav.Link href="#LogIn">Log In</Nav.Link>
           <Button variant="primary">Sign Up</Button>{' '}
           </Nav>
         </Container>
